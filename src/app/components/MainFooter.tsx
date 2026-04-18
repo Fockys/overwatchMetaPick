217e9c4437c8aea0ae9d3844f305aece5c4327ee
Focky <72989848+Fockys@users.noreply.github.com>
2026-04-18 23:28:52 +0100

    reduced footer size

--- src/app/components/MainFooter.tsx
+++ src/app/components/MainFooter.tsx
@@ -3,8 +3,8 @@ import "@/app/globals.css"
 
 export default function MainFooter(){
     return(
-        <div id="Footer" className="flex items-center bg-blue-950 w-full p-3 sm:p-4  flex-col ">
-        <div className="sm:w-6/12 w-full h-full justify-around flex-row flex *:text-sm *:sm:text-xl">
+        <div id="Footer" className="flex items-center bg-blue-950 w-full p-1 xxl:p-4  flex-col ">
+        <div className="sm:w-6/12 w-full h-full justify-around flex-row flex *:text-sm *:xxl:text-xl">
             <h1>About</h1>
             <h1>Last updated: now</h1>
             <h1>Github</h1>
