0e38afa022eadc0759395946400c208984a6e500
Focky <72989848+Fockys@users.noreply.github.com>
2026-04-18 22:12:11 +0100

    added password to db

--- src/db/index.ts
+++ src/db/index.ts
@@ -10,6 +10,7 @@ try{
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
   });
   db = drizzle(connection)
 }
