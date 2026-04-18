5673355ae824c5a555aabf7beb3540a2c2eb816e
Focky <72989848+Fockys@users.noreply.github.com>
2026-04-18 23:37:54 +0100

    added github link to footer

--- src/app/components/MainFooter.tsx
+++ src/app/components/MainFooter.tsx
@@ -5,9 +5,9 @@ export default function MainFooter(){
     return(
         <div id="Footer" className="flex items-center bg-blue-950 w-full p-1 xxl:p-4  flex-col ">
         <div className="sm:w-6/12 w-full h-full justify-around flex-row flex *:text-sm *:xxl:text-xl">
-            <h1>About</h1>
-            <h1>Last updated: now</h1>
-            <h1>Github</h1>
+            <a>About</a>
+            <a>Last updated: now</a>
+            <a href="https://github.com/Fockys/overwatchMetaPick">Github</a>
         </div>
         </div>
 
