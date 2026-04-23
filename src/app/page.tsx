6a1872e35e9abe9d7c121c9e036e9b5a4b7ad97a
Focky <72989848+Fockys@users.noreply.github.com>
2026-04-23 16:13:14 +0100

    added darkening to landing background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,15 @@ export default async function Home() {
 
 
   return (
-    <div className="bg-[url(/images/gibraltar.jpg)] h-full  bg-cover bg-center overflow-hidden touch-none flex flex-col items-center pt-[9vh] sm:pt-[12vh]">
-      <h1 className="text-5xl sm:text-8xl text-white font-[overwatchFont] ">Find your next pick</h1>
+    <div className=" relative h-full overflow-hidden touch-none">
+
+    <div className="bg-[url(/images/gibraltar.jpg)] opacity-90 h-full absolute inset-0  bg-cover bg-center"/>
+    <div className="absolute inset-0 bg-black/20" />
+
+    <div className="relative z-10 flex flex-col items-center pt-[9vh] sm:pt-[12vh]">
+      <h1 className="text-5xl sm:text-8xl text-white font-[overwatchFont]">Find your next pick</h1>
       <HeroSearch heros={heros} className=" sm:w-9/12 lg:w-180  w-[94vw]"></HeroSearch>
+    </div>
     </div>
   );
 }
