d6a4073f0244de72cba84173dff050a53baf5570
Focky <72989848+Fockys@users.noreply.github.com>
2026-04-27 13:21:36 +0100

    implemented all delete actions

--- src/app/dashboard/actions/deleteActions.ts
+++ src/app/dashboard/actions/deleteActions.ts
@@ -1,10 +1,24 @@
 "use server"
 
 import { db } from "@/db";
-import { abilityTable } from "@/db/schema";
+import { abilityTable, countersTable, passivesTable, weaponsTable } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
 
-export default async function deleteAbility(abilityID:number){
+export async function deleteAbility(abilityID:number){
     await db.delete(abilityTable).where(eq(abilityTable.id,abilityID))
 }
+
+export async function deletePassive(passiveID:number){
+    await db.delete(passivesTable).where(eq(passivesTable.id,passiveID))
+}
+
+export async function deleteWeapon(weaponID:number){
+    await db.delete(weaponsTable).where(eq(weaponsTable.id,weaponID))
+}
+
+export async function deleteCounter(counterID:number){
+    await db.delete(countersTable).where(eq(countersTable.id,counterID))
+}
+
+//function for delete hero shouldnt be implemented as this shouldnt normally happen
