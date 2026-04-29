d7d74a0bfa3ffa7ab8cb2026925a1b9a1414770d
Focky <72989848+Fockys@users.noreply.github.com>
2026-04-29 19:41:16 +0100

    added more post actions

--- src/app/dashboard/actions/postActions.ts
+++ src/app/dashboard/actions/postActions.ts
@@ -1,7 +1,7 @@
 "use server"
 import { db } from "@/db";
-import { abilityTable, heroTable } from "@/db/schema";
-import { eq } from "drizzle-orm";
+import { abilityTable, countersTable, heroTable, passivesTable, weaponsTable } from "@/db/schema";
+import { and, desc, eq } from "drizzle-orm";
 
 
 
@@ -30,21 +30,37 @@ export async function editAbility(abilityID:number, name:string, description:str
 
 //counters
 export async function createCounter(heroID:number, heroCounterID:number, reason:string){
+    await db.insert(countersTable)
+    .values({heroID:heroID,heroCounterID:heroCounterID,reason:reason})
 }
 
-export async function editCounter(counterID:number, HeroCounterID:number, reason:string){
+export async function editCounter(counterID:number, heroCounterID:number, reason:string){
+    await db.update(countersTable)
+    .set({heroID:counterID,heroCounterID:heroCounterID,reason:reason})
+    .where(and(eq(countersTable.heroID,counterID),eq(countersTable.heroCounterID,heroCounterID)));
+
 }
 
 //passives
 export async function createPassive(name:string, description:string){
+    await db.insert(passivesTable)
+    .values({name:name,description:description})
 }
 
 export async function editPassive(passiveID:number, name:string, description:string){
+    await db.update(passivesTable)
+    .set({name:name,description:description})
+    .where(eq(passivesTable.id,passiveID))
 }
 
 //weapons
 export async function createWeapon(heroID:number, name:string, description:string){
+    await db.insert(weaponsTable)
+    .values({heroID:heroID, name:name, description:description});
 }
 
 export async function editWeapon(weaponID:number, name:string, description:string){
+    await db.update(weaponsTable)
+    .set({name:name,description:description})
+    .where(eq(weaponsTable.id,weaponID));
 }
